a5684a45690c164014d1f921e824e32a26f4e2dc
ShokhsanamShirinkulova <33857100+ShokhsanamShirinkulova@users.noreply.github.com>
2020-01-31 14:24:14 +0500

    abc

--- abooDemo/settings.py
+++ abooDemo/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'abooDemo.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='postgres://popwkxxvuuyiyk:8c97d57b68e934714cc1183d76c68a7f16f7c0281522a0fdef54310bd218dfab@ec2-54'
-                '-225-242-183.compute-1.amazonaws.com:5432/d9jdj8jnt3i4au '
+                '-225-242-183.compute-1.amazonaws.com:5432/d9jdj8jnt3i4au'
     )
 }
 DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
